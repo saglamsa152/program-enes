8b297db4353285bfe129dfec2f997b7c9e52ee6d
saglamsa152 <32458548+saglamsa152@users.noreply.github.com>
2017-10-02 19:52:39 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,toplam;
+                    Console.Write ("ilk değeri giriniz");
+            a=Convert.ToInt16(Console.ReadLine());
+           Console.Write ("ikinci değeri giriniz");
+            b=Convert.ToInt16(Console.ReadLine());
+            toplam=a + b;
+            Console.WriteLine();
+            Console.WriteLine("{0}ve {1} sayıların toplamı: {2}",a,b,(a+b));
+            Console.ReadKey();
+
+        }
+    }
+}
